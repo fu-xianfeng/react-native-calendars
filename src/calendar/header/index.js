894aff750dcc06960b6a0c0715fb6544a540671a
fu-xianfeng <gray_al_fu@163.com>
2018-05-11 11:20:32 +0800

    Update index.js

--- src/calendar/header/index.js
+++ src/calendar/header/index.js
@@ -113,9 +113,9 @@ class CalendarHeader extends Component {
         <View style={this.style.header}>
           {leftArrow}
           <View style={{ flexDirection: 'row' }}>
-//             <Text allowFontScaling={false} style={this.style.monthText} accessibilityTraits='header'>
-//               {this.props.month.toString(this.props.monthFormat ? this.props.monthFormat : 'MMMM yyyy')}
-//             </Text>
+            {/*<Text allowFontScaling={false} style={this.style.monthText} accessibilityTraits='header'>*/}
+               {/*{this.props.month.toString(this.props.monthFormat ? this.props.monthFormat : 'MMMM yyyy')}*/}
+             {/*</Text>*/}
             {indicator}
           </View>
           {rightArrow}
